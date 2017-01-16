34c2b7f7260aa04967b41a730533c27baa973463
Tobia Di Pisa <tobia.dipisa@geo-solutions.it>
2017-01-16 12:30:34 +0100

    modifying setup files

--- setup.py
+++ setup.py
@@ -1,22 +1,48 @@
-from setuptools import setup, find_packages
-import sys
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
 
-version = '0.1.1'
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='ckanext-datitrentinoit',
-    version=version,
+    version = '0.1.1',
     description="CKAN customizations for dati.trentino.it",
     long_description="CKAN customizations for dati.trentino.it",
     author="Tobia Di Pisa",
     author_email="tobia.dipisa@geo-solutions.it",
     url='http://dati.trentino.it',
-    license='Affero GPL',
-    classifiers=[],
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    license='AGPL',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        # 3 - Alpha
+        # 4 - Beta
+        # 5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     namespace_packages=['ckanext', 'ckanext.datitrentinoit'],
     include_package_data=True,
-    zip_safe=False,
+    package_data={
+    },
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[],
     install_requires=[],
     entry_points='''
         [ckan.plugins]
